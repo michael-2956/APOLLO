8fb10768933b88ccb4f49c0564aa6e105c318485
michael-2956 <work.michael.2956@gmail.com>
2025-11-13 19:00:44 +0200

    comment

--- github_test.py
+++ github_test.py
@@ -7,7 +7,7 @@ import Aesop
 
 set_option maxHeartbeats 0
 
-open BigOperators Real Nat Topology Rat
+-- open BigOperators Real Nat Topology Rat
 
 theorem mathd_algebra_141 (a b : ℝ) (h₁ : a * b = 180) (h₂ : 2 * (a + b) = 54) :
     a ^ 2 + b ^ 2 = 369 := by
