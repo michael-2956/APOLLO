34ea384262ef2f6d0184cbaf0499fe57ee0c5a04
michael-2956 <work.michael.2956@gmail.com>
2025-11-14 12:59:18 +0200

    add print

--- utils/sorrify.py
+++ utils/sorrify.py
@@ -398,10 +398,10 @@ class Sorrifier:
 
         print(f"ERR_INFO:\n\n\n\n{err_info = }\n\n\n\n")
 
-        print(f"Other info:\n\n\n\n{attempt_num = }\n{to_check = }\n{self.proof_tree.code.splitlines()[main_goal:] = }\n\n\n\n")
+        print(f"Other info:\n\n\n\n{attempt_num = }\n{to_check = }\n{self.proof_tree.code.splitlines() = }\n{self.proof_tree.code.splitlines()[main_goal:] = }\n\n\n\n")
 
         # assert attempt_num > 0
-        
+
         if err_info['pass']:
             return self.proof_tree.code
         
