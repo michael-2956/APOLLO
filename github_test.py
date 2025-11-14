97cbe30e2e453cc91c5284ccb14682302fd21087
michael-2956 <work.michael.2956@gmail.com>
2025-11-14 13:21:08 +0200

    different code

--- github_test.py
+++ github_test.py
@@ -39,9 +39,6 @@ theorem mathd_algebra_141 (a b : ℝ) (h₁ : a * b = 180) (h₂ : 2 * (a + b) =
     a ^ 2 + b ^ 2 = 369 := by
   have h3 : a + b = 27 := by
     linarith
-  have h4 : (a + b) ^ 2 = 729 := by
-    rw [h3]
-    norm_num
   linarith
 '''
 
